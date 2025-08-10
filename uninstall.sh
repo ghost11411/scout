a387d7bc9e959ee3610b3798272b4dab561df673
Sourav Mishra <souravm297@gmail.com>
2025-08-11 04:21:19 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
-
 # VARS
 RESET="\e[0m"
-OKBOLD="\033[01;01m"
-OKRED="\033[0;31m"
-OKGREEN="\033[0;32m"
-OKBLUE="\033[1;34m"
-
-#CHECK ROOT
-echo 
-echo -e "${OKBLUE} Checking Root Permissions ${RESET}"
-sleep 2
-if [ "$EUID" -ne 0 ];then 
-    echo -e "${OKRED} !!Please run as root (use sudo ./install.sh)!! ${RESET}"
-exit
-else
-    echo -e "${OKGREEN} **Root Permission Granted** ${RESET}"
-fi
-echo
+BOLD="\033[01;01m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+BLUE="\033[1;34m"
 
 INSTALL_DIR=~/scout
 
-echo -e "${OKBOLD}${OKBLUE}[This script will uninstall scout and remove ALL files under $INSTALL_DIR. Are you sure you want to continue?"
+#CHECK ROOT
+echo 
+echo -e "${BLUE}Checking Root Permissions ${RESET}"
+sleep 0.5
+if [ "$EUID" -ne 0 ];then 
+    echo -e "${BOLD}${RED}!!Please run as root (sudo ./uninstall.sh)!! ${RESET}"
+exit
+else
+    echo -e "${GREEN} **Root Permission Granted** ${RESET}"
+fi
+echo
+
+echo -e "${BOLD}${BLUE}[This script will uninstall scout and remove ALL files under $INSTALL_DIR. Are you sure you want to continue?"
 read -p "(Y/N): " answer
 
 if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
     sudo rm -Rf ~/scout/
     clear
-    echo -e "${OKBOLD}${OKGREEN}[ Removed ]${RESET}"
+    echo -e "${BOLD}${GREEN}[Scout is Removed]${RESET}"
 else 
-    echo -e "${OKBOLD}${OKRED}[ Not Removed ]${RESET}"
+    clear
+    echo -e "${BOLD}${RED}[ Scout is Not Removed ]${RESET}"
 fi
