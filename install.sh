c2bbafa015a7996f2f59f23746e5dc75ae800e78
Sourav Mishra <souravm297@gmail.com>
2025-08-11 05:04:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,188 +1,154 @@
 #!/bin/bash
-
-VERSION=1.0
-
-#VARS
+#COLORS
 RESET="\e[0m"
 OKBOLD="\033[01;01m"
 OKRED="\033[0;31m"
 OKGREEN="\033[0;32m"
 OKBLUE="\033[1;34m"
 
-CURRENTDATE=`date +"%Y_-%m_-%d_%T"`
-
-#DIRECTORIES
-INSTALL_DIR=/root/scout
-TOOLS_DIR="$INSTALL_DIR"/tools
-BIN_DIR="$TOOLS_DIR"/bin
-WORDLISTS_DIR="$TOOLS_DIR"/wordlists
-WORKSPACE_DIR="$INSTALL_DIR"/workspace
-OUTPUT_DIR="$WORKSPACE_DIR"/"scout_output_$CURRENTDATE"
-SUBDOMAINS_DIR="$OUTPUT_DIR"/subdomains
-
-domain=''
-
-BANNER(){
-  echo -e "${OKBOLD}${OKORANGE}" 
-  echo -e "${OKBOLD}                      _   "
-  echo -e "${OKBOLD}                     | |  "
-  echo -e "${OKBOLD}  ___  ___ ___  _   _| |_ "
-  echo -e "${OKBOLD} / __|/ __/ _ \| | | | __|"
-  echo -e "${OKBOLD} \__ \ (_| (_) | |_| | |_ "
-  echo -e "${OKBOLD} |___/\___\___/ \__,_|\__|${RESET}"
-  echo -e "${OKBOLD}               :-By Ghost"
-  echo -e "${RESET}"
-  echo ""
-  echo -e "${OKBOLD} LEGENDS:"
-  echo -e "${OKBOLD}${OKBLUE} Blue = Script Running ${RESET}"
-  echo -e "${OKBOLD}${OKGREEN} Green = Everything Fine ${RESET}"
-  echo -e "${OKBOLD}${OKRED} Red = ERROR ${RESET}"
-}
-
-USAGE(){
-	while read -r line; do
-		printf "%b\n" "$line"
-	done <<-EOF
-	${OKRED}\r
-	\r${OKBOLD}${OKBLUE}Options${RESET}:
-	\r    -df         - File of Domains [Required]
-	\r    -h          - Displays this help message and exit.
-	\r    -v          - Displays the version and exit.
-
-	\r${OKBOLD}${OKBLUE}Examples${RESET}: 
-	\r    - To run scout.sh against a list of domains:
-	\r    ./scout.sh -df domains.txt
-EOF
-	exit 1
-}
-
-# IF NO ARGS, SHOW USAGE
-if [ $# -eq 0 ]; then
-    BANNER
-    USAGE
-    exit 1
-fi
-
 #Basic Checks
-CHECKS(){
-  echo -e 
-  echo -e "${OKBOLD}${OKBLUE} [*] Running Script ${RESET}"
-  echo 
-  sleep 0.5
+echo -e 
+echo -e "${OKBOLD}${OKBLUE} [*] Running Script ${RESET}"
+echo 
+sleep 0.5
 
 #CHECK ROOT
-  echo -e "${OKBLUE} Checking Root Permissions ${RESET}"
-  sleep 0.5
-  if [ "$EUID" -ne 0 ];then 
-      echo -e "${OKRED} !!Please run as root (use sudo ./install.sh)!! ${RESET}"
-  exit
-  else
-      echo -e "${OKGREEN} **Root Permission Granted** ${RESET}"
-  fi
-  echo
+echo -e "${OKBLUE} Checking Root Permissions ${RESET}"
+sleep 0.5
+if [ "$EUID" -ne 0 ];then 
+    echo -e "${OKRED} !!Please run as root (use sudo ./install.sh)!! ${RESET}"
+exit
+else
+    echo -e "${OKGREEN} **Root Permission Granted** ${RESET}"
+fi
+echo
 
-  #CHECK INTERNET
-  echo -e "${OKBLUE} Checking Internet Connection ${RESET}"
-  wget -q --spider http://google.com
-  if [ $? -eq 0 ]; then
-      echo -e "${OKGREEN} **Online** ${RESET}"
-  else
-      echo -e "${OKRED} !!Offline!! ${RESET}"
-      echo -e "${OKRED} !!Connect to Internet and rerun the script!! ${RESET}"
-      exit
-  fi
-  sleep 0.5
-  # clear
+#CHECK INTERNET
+echo -e "${OKBLUE} Checking Internet Connection ${RESET}"
+wget -q --spider http://google.com
+if [ $? -eq 0 ]; then
+    echo -e " ${OKGREEN} **Online** ${RESET}"
+else
+    echo -e "${OKRED} !!Offline!! ${RESET}"
+    echo -e "${OKRED} !!Connect to Internet and rerun the script!! ${RESET}"
+    exit
+fi
+sleep 2
+clear
+
+#DIRECTORIES
+INSTALL_DIR=~/scout
+TOOLS_DIR="$INSTALL_DIR"/tools
+BIN_DIR="$TOOLS_DIR"/bin
+WORDLISTS_DIR="$TOOLS_DIR"/wordlists/
+
+#DOWNLOAD AND INSTALL TOOLS
+function install_tools {
+    echo -e "${OKBOLD}${OKBLUE} Making Folders ${RESET}"
+    mkdir -p "$BIN_DIR" && chmod 777 -Rf "$BIN_DIR"
+    mkdir -p ~/.gf
+    mkdir -p "$WORDLISTS_DIR"
+    sleep 2
+    echo -e "${OKBOLD}${OKBLUE} Installing Tools ${RESET}"
+    apt install -y whois jq unzip massdns sqlmap python3 python3-pip python3-argparse python3-requests python3-dnsython git golang-go gobuster parallel &> /dev/null
+    echo -e "${OKBLUE}  Installing GO" && wget -q -O - https://git.io/vQhTU | bash &>/dev/null
+    sleep 2s
+    echo -e "${OKBLUE}  Installing mapcidr" && go install github.com/projectdiscovery/mapcidr/cmd/mapcidr@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing assetfinder" && go install github.com/tomnomnom/assetfinder@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing subfinder" && go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing waybackurls" && go install github.com/tomnomnom/waybackurls@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing amass" && go install github.com/owasp-amass/amass/v4/...@master &> /dev/null
+    echo -e "${OKBLUE}  Installing dnsx" && go install github.com/projectdiscovery/dnsx/cmd/dnsx@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing gf" && go install github.com/tomnomnom/gf@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing gauplus" && go install github.com/bp0lr/gauplus@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing unfurl" && go install github.com/tomnomnom/unfurl@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing shuffledns" && go install github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing puredns" && go install github.com/d3mondev/puredns/v2@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing httprobe" && go install github.com/tomnomnom/httprobe@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing httpx" && go install github.com/projectdiscovery/httpx/cmd/httpx@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing ffuf" && go install github.com/ffuf/ffuf/v2@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing subjs" && go install github.com/lc/subjs@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing nuclei" && go install github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing subjack" && go install github.com/haccer/subjack &> /dev/null
+    echo -e "${OKBLUE}  Installing naabu" && go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing kxss" && go install github.com/Emoe/kxss@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing notify" && go install -v github.com/projectdiscovery/notify/cmd/notify@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing dalfox" && go install github.com/hahwul/dalfox/v2@latest &> /dev/null
+    echo -e "${OKBLUE}  Installing findomain"
+    FINDD_VER=$(curl -sL https://api.github.com/repos/Findomain/Findomain/releases/latest | jq -r ".tag_name")
+    wget "https://github.com/Findomain/Findomain/releases/download/$FINDD_VER/findomain-linux.zip" -O /tmp/findomain.zip &> /dev/null
+    sudo unzip /tmp/findomain.zip -d $BIN_DIR &> /dev/null
+    echo -e "${OKBLUE}  Installing sublister"
+    git clone https://github.com/aboul3la/Sublist3r.git $BIN_DIR/sublister &> /dev/null &> /dev/null
+    echo -e "${OKBLUE}  Installing sqlmap"
+    git clone https://github.com/sqlmapproject/sqlmap.git $BIN_DIR/sqlmap &> /dev/null 
+    mv ~/go/bin/* ${BIN_DIR}
+    echo -e "${OKBLUE} Getting GF Patterns"
+    # cp -r ~/go/src/github.com/tomnomnom/gf/examples ~/.gf
+    git clone https://github.com/1ndianl33t/GF-Patterns ~/.gf &> /dev/null
+    echo -e "${OKBLUE} Updating Nuclie Templates"
+    nuclei -update-templates &> /dev/null
+    echo -e "${OKBOLD}${OKGREEN} All Tools Installed ${RESET}"
+    echo -e
+    echo -e "${OKBOLD}${OKBLUE} Getting Wordlists ${RESET}"
+    wget -q https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/deepmagic.com-prefixes-top50000.txt -O $WORDLISTS_DIR/subdomains.txt &> /dev/null
+    wget -q https://raw.githubusercontent.com/janmasarik/resolvers/master/resolvers.txt -O $WORDLISTS_DIR/resolvers.txt &> /dev/null
+    wget -q https://raw.githubusercontent.com/Bo0oM/fuzz.txt/master/fuzz.txt -O $WORDLISTS_DIR/fuzz.txt &> /dev/null
+    wget -q https://github.com/danielmiessler/SecLists/blob/master/Discovery/DNS/dns-Jhaddix.txt -O $WORDLISTS_DIR/dns.txt &> /dev/null
+    wget -q https://github.com/danielmiessler/SecLists/blob/master/Discovery/Web-Content/big.txt -O $WORDLISTS_DIR/big.txt &> /dev/null
+    wget -q https://wordlists-cdn.assetnote.io/data/manual/2m-subdomains.txt -O "$WORDLISTS_DIR" &>/dev/null
+    wget -q https://wordlists-cdn.assetnote.io/data/manual/best-dns-wordlist.txt -O "$WORDLISTS_DIR" &>/dev/null
+    wget -q https://gist.githubusercontent.com/jhaddix/86a06c5dc309d08580a018c66354a056/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt -O "$WORDLISTS_DIR" &>/dev/null
+    echo -e
+    chmod 777 $BIN_DIR/*
 }
 
-#CREATE FOLDERS 
-function create_folders {
-  mkdir -p "$OUTPUT_DIR"
-  # if [ -d "$OUTPUT_DIR" ]; then
-  #   echo "$OUTPUT_DIR does exist."
-  # fi
-  mkdir -p "$SUBDOMAINS_DIR"
-  # if [ -d "$SUBDOMAINS_DIR" ]; then
-  #   echo "$SUBDOMAINS_DIR does exist."
-  # fi
-  echo
-}
+echo -e "${OKBOLD}${OKGREEN} Scout will be Installed in "$HOME/scout/" ${RESET}"
+echo -e "${OKBLUE} [*] Select Input to Proceed"
+echo -e "${OKBLUE}  1. Install Scout"
+echo -e "${OKBLUE}  2. Complete Reinstall"
+# echo -e "${OKBLUE}  3. Complete Reinstall (Backup Workplace Folder)"
+echo -e ""
+read -p "Selection: "
+echo -e "${RESET}"
 
-function collect_subdomains {
-    while read -r domain; do
-        echo -e "${OKBOLD}${OKBLUE} !!Checking $domain!! ${RESET}"
-        echo "${OKBOLD}${OKBLUE}#### Running Amass ####${RESET}"
-        sudo "$AMASS_BIN" enum -passive -timeout 5 -d "$domain" -o "$SUBDOMAINS_DIR"/amass$domain.out &>/dev/null
-        echo -e "${OKBOLD}${OKGREEN}""[+] Amass Found:${RESET} $(wc -l < "$SUBDOMAINS_DIR"/amass$domain.out)"
-        echo 
-        # echo -e "${OKBOLD}${OKBLUE}#### Running SubFinder #### ${RESET}"
-        # echo "$domain" | "$BIN_DIR"/subfinder -all -silent | sort -u > "$SUBFINDER_OUT"
-        # echo -e "${OKBOLD}${OKGREEN}[+] SubFinder Found:${RESET} $(wc -l "$SUBFINDER_OUT")"
-        # echo
-        # echo -e "${OKBOLD}${OKBLUE}#### Running AssetFinder ####${RESET}"
-        # echo "$domain" | "$BIN_DIR"/assetfinder -subs-only | sort -u > "$ASSETFINDER_OUT"
-        # echo -e "${OKBOLD}${OKGREEN}[+] AssetFinder Found:${RESET} $(wc -l "$ASSETFINDER_OUT")"
-        # echo
-        # echo -e "${OKBOLD}${OKBLUE}#### Running FindDomain ####${RESET}"
-        # "$BIN_DIR"/findomain -q -t "$domain" -u > "$FINDDOMAIN_OUT"
-        # echo -e "${OKBOLD}${OKGREEN}[+] FinDomain Found:${RESET} $(wc -l "$FINDDOMAIN_OUT")"
-        # echo
-        # echo -e "${OKBOLD}${OKBLUE}#### Running GAUPLUS ####${RESET}"
-        # echo "$domain" | "$BIN_DIR"/gauplus | $BIN_DIR/unfurl domain | sort -u > "$GAUPLUS_OUT"
-        # echo -e "${OKBOLD}${OKGREEN}[+] GAUPLUS Found:${RESET} $(wc -l "$GAUPLUS_OUT")"
-        # echo         
-        # echo -e "${OKBOLD}${OKBLUE}#### Running WAYBACKURLS ####${RESET}"
-        # echo "$domain" | "$BIN_DIR"/waybackurls | $BIN_DIR/unfurl domain | sort -u > "$WAYBACKURLS_OUT"
-        # echo -e "${OKBOLD}${OKGREEN}[+] WAYBACKURLS Found:${RESET} $(wc -l "$WAYBACKURLS_OUT")"
-        # echo
-        # curl -sk "http://web.archive.org/cdx/search/cdx?url=*.$domain&output=txt&fl=original&collapse=urlkey&page=" | awk -F/ '{gsub(/:.*/, "", $3); print $3}' | sort -u >"$WEBARCHIVE_OUT"
-        # echo -e "${OKBOLD}${OKGREEN}[+] WEBARCHIVE Found:${RESET} $(wc -l "$WEBARCHIVE_OUT")"
-        # curl -sk "https://crt.sh/?q=%.$domain&output=json" | tr ',' '\n' | awk -F'"' '/name_value/ {gsub(/\*\./, "", $4); gsub(/\\n/,"\n",$4);print $4}' | sort -u > "$CRTSH_OUT"
-        # echo -e "${OKBOLD}${OKGREEN}[+] Crt Found:${RESET} $(wc -l "$CRT_OUT")"
-    done < "$WILDCARDS"
-}
-
-# # SORT ALL SUB-DOMAINS
-# function run_sort {
-#   echo "${OKBOLD}${OKBLUE}#### Sorting Files ####${RESET}"
-#   touch "$COLLECTED"
-#   cat "$RAW_DIR"/*.out | $ANEW_BIN "$COLLECTED" &>/dev/null 
-#   sort -u "$COLLECTED" > "$COLLECTED_SORTED"
-#   echo -e "${OKGREEN}""[+] All Collected:${RESET} $(wc -l < $COLLECTED)"
-#   #grep -F -vf  "$OUT_SCOPE" "$COLLECTED_SORTED" > "$COLLECTED_FINAL"
-#   echo
-# }
-
-# function passive_recursive {
-#     for sub in $( ( cat "$COLLECTED" | rev | cut -d '.' -f 3,2,1 | rev | sort | uniq -c | sort -nr | grep -v '1 ' | head -n 10 && cat "$COLLECTED" | rev | cut -d '.' -f 4,3,2,1 | rev | sort | uniq -c | sort -nr | grep -v '1 ' | head -n 10 ) | sed -e 's/^[[:space:]]*//' | cut -d ' ' -f 2);do 
-#         "$SUBFINDER_BIN" -d "$sub" -silent -max-time 2 | anew -q passive_recursive.txt
-#         "$ASSETFINDER_BIN" --subs-only "$sub" | anew -q passive_recursive.txt
-#         "$AMASS_BIN" enum -timeout 5 -passive -d "$sub" | anew -q passive_recursive.txt
-#         "$FINDOMAIN_BIN" --quiet -t "$sub" | anew -q passive_recursive.txt
-#     done
-# }
-
-while [ -n "$1" ]; do
-	case $1 in
-		-df)
-			WILDCARDS=$2
-      BANNER
-      CHECKS
-      create_folders
-      collect_subdomains
-			shift ;;
-		-h|--help)
-      BANNER
-			USAGE;;
-		-v|--version)
-      BANNER
-      echo -e
-			echo "Current Version is v$VERSION"
-			exit 0 ;;
-		*)
-			echo "[-] Unknown Option: $1"
-      BANNER
-			USAGE ;;
-	esac
-	shift
-done
+case "${REPLY}" in
+    
+    1)  echo -e "${OKBOLD}${OKBLUE}## Installing Scout ## ${RESET}"
+        echo
+        if [ ! -d "$INSTALL_DIR" ]; then
+            mkdir -p "$INSTALL_DIR" && chmod 777 -Rf "$INSTALL_DIR"
+            git clone https://github.com/ghost11411/scout.git -q "$INSTALL_DIR"
+            install_tools
+            chmod 777 "$INSTALL_DIR"/*
+            echo -e "${OKBOLD}${OKBLUE}## Installation Completed ## ${RESET}"
+        else 
+            echo -e "{$OKBOLD}${OKRED}[Installation Folder Already Exists] ${RESET}"
+            echo -e "{$OKBOLD}${OKRED}[If you want to reinstall, rerun the script again and select option 2 or 3] ${RESET}"
+        fi
+        echo;;
+    
+    2)  echo -e "${OKBOLD}${OKBLUE}## Complete Reinstalling Scout ## ${RESET}"
+        echo
+        echo -e "${OKBOLD}${OKBLUE}[Removing Scout Folder]${RESET}"
+        rm -r "$INSTALL_DIR" && mkdir -p "$INSTALL_DIR" && chmod 777 -Rf "$INSTALL_DIR" &>/dev/null
+        echo -e "${OKBOLD}${OKBLUE}[Downloading Latest Files]${RESET}"
+        git clone https://github.com/ghost11411/scout -q "$INSTALL_DIR"
+        install_tools
+        chmod 777 "$INSTALL_DIR"/* 
+        echo -e "${OKBOLD}${OKBLUE}## Installation Completed ## ${RESET}" ;;
+    
+    # 3)  echo -e "${OKBOLD}${OKBLUE}##Complete Reinstalling Scout (Backup Workspace Folder)## ${RESET}"
+    #     echo -e "${OKBOLD}${OKBLUE}[Making Backup]"
+    #     cp $WORKSPACE_DIR ~/
+    #     echo -e "${OKBOLD}${OKBLUE}[Removing Scout Folder]"
+    #     rm -r "$INSTALL_DIR" && mkdir -p "$INSTALL_DIR" && chmod 777 -Rf "$INSTALL_DIR"
+    #     echo -e "[Downloading Latest Files]${RESET}"
+    #     git clone https://github.com/ghost11411/scout -q "$INSTALL_DIR"
+    #     install_tools
+    #     cp /tmp/ $WORKSPACE_DIR
+    #     chmod 777 "$INSTALL_DIR"/* ;;
+    
+    *)  echo "Invalid Input"
+        ;;
+esac
