d5fe8577c2ba31c82c438d7462c40f7f5459f6ab
Sourav Mishra <souravm297@gmail.com>
2025-08-11 10:25:48 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -8,25 +8,13 @@ BLUE="\033[1;34m"
 
 INSTALL_DIR=~/scout
 
-#CHECK ROOT
-echo 
-echo -e "${BLUE}Checking Root Permissions ${RESET}"
-sleep 0.5
-if [ "$EUID" -ne 0 ];then 
-    echo -e "${BOLD}${RED}!!Please run as root (sudo ./uninstall.sh)!! ${RESET}"
-exit
-else
-    echo -e "${GREEN} **Root Permission Granted** ${RESET}"
-fi
-echo
-
 echo -e "${BOLD}${BLUE}[This script will uninstall scout and remove ALL files under $INSTALL_DIR. Are you sure you want to continue?"
 read -p "(Y/N): " answer
 
 if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
-    sudo rm -Rf ~/scout/
+    sudo rm -Rf $INSTALL_DIR
     clear
-    echo -e "${BOLD}${GREEN}[Scout is Removed]${RESET}"
+    echo -e "${BOLD}${GREEN}[Scout is Removed From $INSTALL_DIR]${RESET}"
 else 
     clear
     echo -e "${BOLD}${RED}[ Scout is Not Removed ]${RESET}"
