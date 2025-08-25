66ad4df8a2da3ee05c6e5bae28fc802c4d070020
Sourav Mishra <souravm297@gmail.com>
2025-08-25 06:31:53 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -46,37 +46,34 @@ echo -e "${OKBOLD}${OKBLUE} 3. Installing Tools & Dependencies${RESET}"
 apt install -y whois jq unzip massdns sqlmap python3 python3-pip python3-argparse python3-requests python3-dnsython pipx git golang-go gobuster parallel &> /dev/null
 wget -q -O - https://git.io/vQhTU | bash &>/dev/null
 sleep 1
-echo -e "${OKBLUE}  Installing mapcidr" && go install github.com/projectdiscovery/mapcidr/cmd/mapcidr@latest &> /dev/null
+# echo -e "${OKBLUE}  Installing mapcidr" && go install github.com/projectdiscovery/mapcidr/cmd/mapcidr@latest &> /dev/null
+echo -e "${OKBLUE}  Installing amass" && go install github.com/owasp-amass/amass/v4/...@master &> /dev/null
 echo -e "${OKBLUE}  Installing assetfinder" && go install github.com/tomnomnom/assetfinder@latest &> /dev/null
 echo -e "${OKBLUE}  Installing subfinder" && go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest &> /dev/null
 echo -e "${OKBLUE}  Installing waybackurls" && go install github.com/tomnomnom/waybackurls@latest &> /dev/null
-echo -e "${OKBLUE}  Installing amass" && go install github.com/owasp-amass/amass/v4/...@master &> /dev/null
-echo -e "${OKBLUE}  Installing dnsx" && go install github.com/projectdiscovery/dnsx/cmd/dnsx@latest &> /dev/null
-echo -e "${OKBLUE}  Installing gf" && go install github.com/tomnomnom/gf@latest &> /dev/null
 echo -e "${OKBLUE}  Installing gau" && go install github.com/lc/gau/v2/cmd/gau@latest &> /dev/null
+# echo -e "${OKBLUE}  Installing dnsx" && go install github.com/projectdiscovery/dnsx/cmd/dnsx@latest &> /dev/null
+echo -e "${OKBLUE}  Installing gf" && go install github.com/tomnomnom/gf@latest &> /dev/null
 echo -e "${OKBLUE}  Installing unfurl" && go install github.com/tomnomnom/unfurl@latest &> /dev/null
-echo -e "${OKBLUE}  Installing shuffledns" && go install github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest &> /dev/null
-echo -e "${OKBLUE}  Installing puredns" && go install github.com/d3mondev/puredns/v2@latest &> /dev/null
-echo -e "${OKBLUE}  Installing httprobe" && go install github.com/tomnomnom/httprobe@latest &> /dev/null
+# echo -e "${OKBLUE}  Installing shuffledns" && go install github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest &> /dev/null
+# echo -e "${OKBLUE}  Installing puredns" && go install github.com/d3mondev/puredns/v2@latest &> /dev/null
+# echo -e "${OKBLUE}  Installing httprobe" && go install github.com/tomnomnom/httprobe@latest &> /dev/null
 echo -e "${OKBLUE}  Installing httpx" && go install github.com/projectdiscovery/httpx/cmd/httpx@latest &> /dev/null
-echo -e "${OKBLUE}  Installing ffuf" && go install github.com/ffuf/ffuf/v2@latest &> /dev/null
-echo -e "${OKBLUE}  Installing subjs" && go install github.com/lc/subjs@latest &> /dev/null
+# echo -e "${OKBLUE}  Installing ffuf" && go install github.com/ffuf/ffuf/v2@latest &> /dev/null
+# echo -e "${OKBLUE}  Installing subjs" && go install github.com/lc/subjs@latest &> /dev/null
 echo -e "${OKBLUE}  Installing nuclei" && go install github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest &> /dev/null
-echo -e "${OKBLUE}  Installing subjack" && go install github.com/haccer/subjack@latest &> /dev/null
-echo -e "${OKBLUE}  Installing naabu" && go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest &> /dev/null
-echo -e "${OKBLUE}  Installing kxss" && go install github.com/Emoe/kxss@latest &> /dev/null
-echo -e "${OKBLUE}  Installing notify" && go install -v github.com/projectdiscovery/notify/cmd/notify@latest &> /dev/null
-echo -e "${OKBLUE}  Installing dalfox" && go install github.com/hahwul/dalfox/v2@latest &> /dev/null
-echo -e "${OKBLUE}  Installing Arjun" && pipx install arjun &> /dev/null
+# echo -e "${OKBLUE}  Installing subjack" && go install github.com/haccer/subjack@latest &> /dev/null
+# echo -e "${OKBLUE}  Installing naabu" && go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest &> /dev/null
+# echo -e "${OKBLUE}  Installing kxss" && go install github.com/Emoe/kxss@latest &> /dev/null
+# echo -e "${OKBLUE}  Installing notify" && go install -v github.com/projectdiscovery/notify/cmd/notify@latest &> /dev/null
+# echo -e "${OKBLUE}  Installing dalfox" && go install github.com/hahwul/dalfox/v2@latest &> /dev/null
+# echo -e "${OKBLUE}  Installing Arjun" && pipx install arjun &> /dev/null
 echo -e "${OKBLUE}  Installing findomain"
 FINDD_VER=$(curl -sL https://api.github.com/repos/Findomain/Findomain/releases/latest | jq -r ".tag_name")
 wget "https://github.com/Findomain/Findomain/releases/download/$FINDD_VER/findomain-linux.zip" -O /tmp/findomain.zip &> /dev/null
 unzip /tmp/findomain.zip -d $BIN_DIR &> /dev/null
 echo -e "${OKBLUE}  Installing sublister"
 git clone https://github.com/aboul3la/Sublist3r.git $BIN_DIR/sublister &> /dev/null &> /dev/null
-echo -e "${OKBLUE}  Installing sqlmap"
-git clone https://github.com/sqlmapproject/sqlmap.git $BIN_DIR/sqlmap &> /dev/null 
-mv ~/go/bin/* ${BIN_DIR}
 echo -e "${OKBOLD}${OKGREEN} **All Tools Installed** ${RESET}"
 echo -e "${OKBLUE} Getting GF Patterns"
 # cp -r ~/go/src/github.com/tomnomnom/gf/examples ~/.gf
